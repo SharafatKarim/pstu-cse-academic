90b4250f13c5c3fa0d2704b02ec944842ca9f7bc
SharafatKarim <sharafat2004@gmail.com>
2024-01-05 09:59:14 +0600

    and here's number 3

--- semester 2/cce OOP/Exception Handling/codes_exception/Problem_03.java
+++ semester 2/cce OOP/Exception Handling/codes_exception/Problem_03.java
@@ -4,6 +4,36 @@
 // Write a program to demonstrate that the catch block for type ExceptionA catches
 // exceptions of types ExceptionB and ExceptionC.
 
-public class Problem_03 {
+class ExceptionA extends Exception {
+    public ExceptionA(String message) {
+        super(message);
+    }
+}
 
+class ExceptionB extends ExceptionA {
+    public ExceptionB(String message) {
+        super(message);
+    }
+}
+
+class ExceptionC extends ExceptionB {
+    public ExceptionC(String message) {
+        super(message);
+    }
+}
+
+public class Problem_03 {
+    public static void main(String[] args) {
+        try {
+            throw new ExceptionB("Exception of type ExceptonB");
+        } catch (ExceptionA e) {
+            System.out.println(e);
+        }
+
+        try {
+            throw new ExceptionC("Exception of type ExceptonC");
+        } catch (ExceptionA e) {
+            System.out.println(e);
+        }
+    }
 }
