666e2f14f267143c6494c723ebc7614324147573
SharafatKarim <sharafat2004@gmail.com>
2024-02-07 20:35:13 +0600

    also 5

--- semester 2/cce OOP/inheritance problemset/five.java
+++ semester 2/cce OOP/inheritance problemset/five.java
@@ -1,1 +1,28 @@
- 
+class Rectangle {
+    int length;
+    int breadth;
+    Rectangle(int x, int y) {
+        length = x;
+        breadth = y;
+    }
+    int area() {
+        return length * breadth;
+    }
+    int perimeter() {
+        return 2 * (length + breadth);
+    }
+}
+
+class square extends Rectangle {
+    square(int x) {
+        super(x, x);
+    }
+}
+
+class Five {
+    public static void main(String[] args) {
+        square s = new square(15);
+        System.out.println("Area of square: " + s.area());
+        System.out.println("Perimeter of square: " + s.perimeter());
+    }
+}
