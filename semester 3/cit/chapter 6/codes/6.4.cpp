8016741d5f2d92c5b8ae7ee57facbe1a0e4ce34c
SharafatKarim <sharafat2004@gmail.com>
2024-06-24 07:40:37 +0600

    Auto push at 07:40:37

--- semester 3/cit/chapter 6/codes/6.4.cpp
+++ semester 3/cit/chapter 6/codes/6.4.cpp
@@ -28,25 +28,35 @@ string toPostfix(queue<string> input_list) {
 }
 
 int main() {
+    // Well we can take input from the user in this way [OPTIONAL]
+    // -------------------------------------------------------------------
     string input;
-    cin >> input;
+    // cin >> input; // If there is space in input this line won't gonna work
+    getline(cin, input); // Taking whole line as input
     queue<string> input_list;
+    string temp_string = "";
     for (int i = 0; i < input.size(); i++) {
-        // input_list.push(string(1, input[i]));
-        if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == '^' || input[i] == '(' || input[i] == ')') {
-            input_list.push(string(1, input[i]));
-        } else {
-            string operand = "";
-            while (input[i] >= 'A' && input[i] <= 'Z') {
-                operand += input[i];
-                i++;
-            }
-            i--;
-            input_list.push(operand);
-        }
+        if (input[i] == ' ') continue; // Avoiding spaces
+        if (isdigit(input[i])) temp_string += input[i];
+        else input_list.push(temp_string), temp_string = "", input_list.push(string(1, input[i]));
     }
+    input_list.push(temp_string);
+
+    // The following lines are to print the queue (input list)
+    // But the queue will be empty later on [WARNING]
+    // while (input_list.size()) {
+    //     cout << input_list.front() << " ";
+    //     input_list.pop();
+    // }
+
+    // Or, We can Directly initialize our queue in this way [OPTIONAL]
+    // -------------------------------------------------------------------
     // queue<string> input_list({"A","+","(","B","*","C","-","(","D","/","E","^","F",")","*","G",")","*","H"});
-    // queue<string> input_list({"A","+","B"});
+    // queue<string> input_list({"A","+","B"}); // Simpler one!
+    
+    // Finally we can call our function toPostfix
+    // -------------------------------------------------------------------
     cout << toPostfix(input_list) << "\n";
+    
     return 0;
 }
