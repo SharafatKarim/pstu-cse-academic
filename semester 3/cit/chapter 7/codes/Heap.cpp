876c8f7248fd919a509413c7f489ad9d0498ad70
SharafatKarim <sharafat2004@gmail.com>
2024-07-07 07:48:21 +0600

    Auto push at 07:48:21

--- semester 3/cit/chapter 7/codes/Heap.cpp
+++ semester 3/cit/chapter 7/codes/Heap.cpp
@@ -33,14 +33,45 @@ public:
             cout << arr[i] << " ";
         } cout << endl;
     }
+    T deleteMax() {
+        T item = arr[1];
+        T last = arr[size];
+        size--;
+        int temp = 1, left = 2, right = 3;
+        while (right <= size) {
+            if (last >= arr[left] && last >= arr[right]) {
+                arr[temp] = last;
+                return item;
+            }
+            if (arr[left] > arr[right]) {
+                arr[temp] = arr[left];
+                temp = left;
+            } else {
+                arr[temp] = arr[right];
+                temp = right;
+            }
+            left = 2 * temp;
+            right = left + 1;
+        }
+        if (left == size && last < arr[left]) {
+            arr[temp] = arr[left];
+            temp = left;
+        }
+        arr[temp] = last;
+        return item;
+    }
 };
 
 int main() {
+    int array [] = {-5, 10, 3, 1, 15, -4, 7, 1};
     Heap<int> heap;
-    heap.insert(10);
-    heap.insert(12);
-    heap.insert(123);
-    heap.insert(24);
+    for (int i=0; i<6; i++) {
+        heap.insert(array[i]);
+    }
     heap.printAll();
+
+    for (int i=0; i<6; i++) {
+        cout << heap.deleteMax() << " ";
+    } cout << endl;
     return 0;
 }
