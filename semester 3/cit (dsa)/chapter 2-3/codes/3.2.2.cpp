9fe411ead485bff75fc2ccec6721461986ef7b81
SharafatKarim <sharafat2004@gmail.com>
2024-11-02 12:42:06 +0600

    Auto push at 12:42:06

--- semester 3/cit (dsa)/chapter 2-3/codes/3.2.2.cpp
+++ semester 3/cit (dsa)/chapter 2-3/codes/3.2.2.cpp
@@ -31,18 +31,29 @@ string eraseABit(string str, int index, int pattern_length) {
     return temp;
 }
 
+string insertABit(string str, int index, string placeholder) {
+    string temp = "";
+    int length = str.length();
+
+    for (int i=0; i <length; i++) {
+        if (i == index) {
+            temp += placeholder;
+        }
+        temp += str[i];
+    }
+
+    return temp;
+}
+
 int main()
 {
-    string str = "To be or not 2B, that is the B,?";
-    string pattern = "B,";
+    string str = "To be or not 2B, that is the?";
+    string pattern = "2B";
+    string placeholder = "to be";
 
     int index = searchPattern(str, pattern);
-
-    while (index != -1)
-    {
-        str = eraseABit(str, index, pattern.length());
-        index = searchPattern(str, pattern);
-    }
+    str = eraseABit(str, index, pattern.length());
+    str = insertABit(str, index, placeholder);
 
     cout << str << endl;
 }
