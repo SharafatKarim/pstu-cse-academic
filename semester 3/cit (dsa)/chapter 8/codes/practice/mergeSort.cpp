3b49b53bd2488efaed072a0c1aaf5ad863603245
SharafatKarim <sharafat2004@gmail.com>
2024-11-03 21:39:46 +0600

    Auto push at 21:39:46

--- semester 3/cit (dsa)/chapter 8/codes/practice/mergeSort.cpp
+++ semester 3/cit (dsa)/chapter 8/codes/practice/mergeSort.cpp
@@ -4,7 +4,32 @@ using namespace std;
 void printArr(vector<int> &arr);
 
 void mergeSortProcess(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp(high-low+1);
+    int i = low, j = mid, k = 0;
+    while (i <= mid-1 && j <= high) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+    while (i <= mid-1) {
+        temp[k++] = arr[i++];
+    }
+    while (j <= high) {
+        temp[k++] = arr[j++];
+    }
+    for (i = low; i <= high; i++) {
+        arr[i] = temp[i-low];
+    }
+}
 
+void mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    mergeSortProcess(arr, low, mid+1, high);
 }
 
 void printArr(vector<int> &arr) {
@@ -18,6 +43,8 @@ int main() {
     cout << "Before sorting: ";
     printArr(arr);
 
+    mergeSort(arr, 0, 4);
+    // mergeSortProcess(arr, 1, 1, 2);
     printArr(arr);
 
     return 0;
