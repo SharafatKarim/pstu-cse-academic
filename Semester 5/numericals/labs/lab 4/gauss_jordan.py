c34ef5848161f726a9517d5fbf69a7d48f7516f8
SharafatKarim <sharafat2004@gmail.com>
2025-08-14 19:02:31 +0600

    Auto push at 19:02:31

---
+++ Semester 5/numericals/labs/lab 4/gauss_jordan.py
@@ -0,0 +1,14 @@
+N = 3
+arr = [[1, 0, 0, 2.2],
+       [0, 2, 0, 2.8],
+       [0, 0, -2.5, -3]]
+
+def print_array(arr, N):
+    for i in range(N):
+        print(arr[i])
+
+def gauss_jordan(arr, N):
+    pass
+
+print_array(arr, N)
+
