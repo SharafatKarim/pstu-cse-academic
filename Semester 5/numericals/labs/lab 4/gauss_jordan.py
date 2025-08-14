defc64372349d9ed1f4bfde357275c033e3520fd
SharafatKarim <sharafat2004@gmail.com>
2025-08-14 20:23:54 +0600

    Auto push at 20:23:54

--- Semester 5/numericals/labs/lab 4/gauss_jordan.py
+++ Semester 5/numericals/labs/lab 4/gauss_jordan.py
@@ -1,14 +1,28 @@
 N = 3
-arr = [[1, 0, 0, 2.2],
-       [0, 2, 0, 2.8],
-       [0, 0, -2.5, -3]]
+arr = [
+    [2, 1, -1, 8],
+    [-3, -1, 2, -11],
+    [-2, 1, 2, -3]
+    ]
 
 def print_array(arr, N):
     for i in range(N):
         print(arr[i])
 
 def gauss_jordan(arr, N):
-    pass
+    for i in range(N):
+        for j in range(N):
+            if i != j:
+                p = arr[j][i] / arr[i][i]
+                for k in range(N+1):
+                    arr[j][k] -= arr[i][k] * p
 
+    for i in range(N):
+        print(arr[i][3] / arr[i][i])
+
+print("Initial matrix : ")
 print_array(arr, N)
+
+print("Solution : ")
+gauss_jordan(arr, N)
 
