4cdabf9a8f9762e18c351dcd4f93375214fcd6c1
SharafatKarim <sharafat2004@gmail.com>
2025-09-20 18:01:46 +0600

    Auto push at 18:01:46

--- Semester 5/AI/algorithms/search/test.py
+++ Semester 5/AI/algorithms/search/test.py
@@ -1,50 +1,17 @@
-from collections import deque
+tree = {
+        'a': ['b', 'c'],
+        'b': ['c', 'e'],
+        'c': ['d'],
+        'd': [],
+        'e': []
+    }
 
-def bidirectional_bfs(graph, start, goal):
-    if start == goal:
-        print("Start and goal are the same:", start)
-        return
+def dfs(graph, start, limit=float('inf')):
+    visited = []
+    if start not in visited:
+        print(start, end=', ')
+    for
 
-    visited_start = set([start])
-    visited_goal = set([goal])
 
-    queue_start = deque([start])
-    queue_goal = deque([goal])
-
-    while queue_start and queue_goal:
-        # Forward BFS step
-        curr_start = queue_start.popleft()
-        for neighbor in graph.get(curr_start, []):
-            if neighbor in visited_goal:
-                print("Path found via:", neighbor)
-                return
-            if neighbor not in visited_start:
-                visited_start.add(neighbor)
-                queue_start.append(neighbor)
-
-        # Backward BFS step
-        curr_goal = queue_goal.popleft()
-        for neighbor in graph.get(curr_goal, []):
-            if neighbor in visited_start:
-                print("Path found via:", neighbor)
-                return
-            if neighbor not in visited_goal:
-                visited_goal.add(neighbor)
-                queue_goal.append(neighbor)
-
-    print("No path found.")
-
-s = input("Graph: ")  # Example: A:B,C B:D,E C:F,G,A D: E:F F:E,C G:C
-graph = {}
-
-for p in s.split():
-    node, neighbors = p.split(":")
-    if neighbors:
-        graph[node] = neighbors.split(",")
-    else:
-        graph[node] = []
-
-start_node = input("Start node: ")
-goal_node = input("Goal node: ")
-
-bidirectional_bfs(graph, start_node, goal_node)
+if __name__ == '__main__':
+    dfs()
