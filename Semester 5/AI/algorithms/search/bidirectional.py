4ace8a893e4395243e7ddb78f533f07bd9486584
SharafatKarim <sharafat2004@gmail.com>
2025-09-20 17:01:31 +0600

    Auto push at 17:01:31

--- Semester 5/AI/algorithms/search/bidirectional.py
+++ Semester 5/AI/algorithms/search/bidirectional.py
@@ -1,13 +1,22 @@
 # Unidriected Tree
-un_tree = {
-        'A': ['B', 'E'],
-        'B': ['C', 'D', 'A'],
-        'C': ['F', 'B'],
-        'D': ['B'],
-        'E': ['A'],
-        'F': ['C']
-    }
+# un_tree = {
+#         'A': ['B', 'E'],
+#         'B': ['C', 'D', 'A'],
+#         'C': ['F', 'B'],
+#         'D': ['B'],
+#         'E': ['A'],
+#         'F': ['C']
+#     }
 
+un_tree = {
+        'A': ['B', 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': [],
+        'F': []
+    }
+# A:B,C B:D,E C:F D: E: F:
 
 from collections import deque
 def bidirectional(tree, start, goal):
@@ -20,22 +29,24 @@ def bidirectional(tree, start, goal):
     start_queue = deque([start])
     goal_queue = deque([goal])
 
-    while start_queue and goal_queue:
-        start_node = start_queue.popleft()
-        if start_node not in start_visited:
-            start_visited.append(start_node)
+    while start_queue or goal_queue:
+        if start_queue:
+            start_node = start_queue.popleft()
+            if start_node not in start_visited:
+                start_visited.append(start_node)
 
-            for neighbour in tree[start_node]:
-                if neighbour not in start_visited:
-                    start_queue.append(neighbour)
+                for neighbour in tree[start_node]:
+                    if neighbour not in start_visited:
+                        start_queue.append(neighbour)
 
-        goal_node = goal_queue.popleft()
-        if goal_node not in goal_visited:
-            goal_visited.append(goal_node)
+        if goal_queue:
+            goal_node = goal_queue.popleft()
+            if goal_node not in goal_visited:
+                goal_visited.append(goal_node)
 
-            for neighbour in tree[goal_node]:
-                if neighbour not in goal_visited:
-                    goal_queue.append(neighbour)
+                for neighbour in tree[goal_node]:
+                    if neighbour not in goal_visited:
+                        goal_queue.append(neighbour)
 
         if start_node in goal_visited or goal_node in start_visited:
             return start_visited, goal_visited
