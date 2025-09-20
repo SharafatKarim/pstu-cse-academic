f2d91f9ec0d63b2b396af9b43d7dacddcbec9685
SharafatKarim <sharafat2004@gmail.com>
2025-09-20 19:20:54 +0600

    Auto push at 19:20:54

--- Semester 5/AI/algorithms/search/test.py
+++ Semester 5/AI/algorithms/search/test.py
@@ -2,16 +2,21 @@ tree = {
         'a': ['b', 'c'],
         'b': ['c', 'e'],
         'c': ['d'],
-        'd': [],
-        'e': []
+        'd': ['e'],
+        'e': ['f'],
+        'f': []
     }
 
 def dfs(graph, start, limit=float('inf')):
+    if limit <= 0:
+        return
     visited = []
     if start not in visited:
         print(start, end=', ')
-    for
+        visited.append(start)
+    for i in graph[start]:
+        dfs(graph, i, limit-1)
 
 
 if __name__ == '__main__':
-    dfs()
+    dfs(tree, 'a', 1)
