39c72c2776f5f813bb25662a1b46c2f0a4718bf8
SharafatKarim <sharafat2004@gmail.com>
2025-09-21 08:36:29 +0600

    Auto push at 08:36:29

--- Semester 5/AI/algorithms/search/test.py
+++ Semester 5/AI/algorithms/search/test.py
@@ -1,3 +1,4 @@
+from collections import deque
 tree = {
         'a': ['b', 'c'],
         'b': ['c', 'e'],
@@ -17,6 +18,22 @@ def dfs(graph, start, limit=float('inf')):
     for i in graph[start]:
         dfs(graph, i, limit-1)
 
+def bfs(graph, start):
+    visited = []
+    queue = deque()
+    queue.pushfront(start)
+
+    while (queue):
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=', ')
+            visited.append(node)
+
+            for i in graph[node]:
+                if i not in visited:
+                    queue.push(i)
 
 if __name__ == '__main__':
     dfs(tree, 'a', 1)
+    # bfs(tree, 'a')
+    print(dir(deque))
